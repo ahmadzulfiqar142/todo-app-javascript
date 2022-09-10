651c85f87c1217f37006f0652e3c0a1b8c5c62b3
Noman-Ijaz <nomiijaz077@gmail.com>
2022-09-10 09:54:48 -0700

    removed styling from js

--- js/index.js
+++ js/index.js
@@ -34,37 +34,6 @@ function addfunction() {
     textList.appendChild(span)
     buttonDiv.appendChild(deletebutton)
     buttonDiv.appendChild(editbutton)
-    // listDiv.style.border = '2px solid gray'
-    listDiv.style.padding = '10px'
-    listDiv.style.display = 'flex'
-    listDiv.style.flexDirection = 'row'
-    listDiv.style.gap = '10px'
-    listDiv.style.justifyContent = 'space-between'
-    listDiv.style.alignItems = 'center'
-    listDiv.style.width = '550px'
-    deletebutton.style.border = 'none'
-    deletebutton.style.padding = ' 12px 30px 12px 30px'
-    deletebutton.style.borderRadius = '10px'
-    deletebutton.style.background = '#ff0000'
-    deletebutton.style.color = 'white'
-    deletebutton.style.fontSize = '15px'
-    deletebutton.style.fontWeight = 'bold'
-    editbutton.style.border = 'none'
-    editbutton.style.padding = ' 12px 30px 12px 30px'
-    editbutton.style.borderRadius = '10px'
-    editbutton.style.background = 'green'
-    editbutton.style.color = 'white'
-    editbutton.style.marginLeft = '10px'
-    editbutton.style.fontSize = '15px'
-    editbutton.style.fontWeight = 'bold'
-    updatebutton.style.border = 'none'
-    updatebutton.style.padding = ' 12px 30px 12px 30px'
-    updatebutton.style.borderRadius = '10px'
-    updatebutton.style.background = 'green'
-    updatebutton.style.color = 'white'
-    updatebutton.style.marginLeft = '10px'
-    updatebutton.style.fontSize = '15px'
-    updatebutton.style.fontWeight = 'bold'
     // delelebutton function
     deletebutton.onclick = function () {
         deletefunction("uniuqeid" + x);
@@ -109,5 +78,4 @@ function updatefunction(id) {
     document.getElementById('txtVal').value = '';
     let b = document.getElementById('upbtn');
     b.remove();
-    console.log(id);
 }
